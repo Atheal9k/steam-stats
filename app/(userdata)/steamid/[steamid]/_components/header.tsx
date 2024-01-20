05c5b25fca3e6d9ac27d7c97852b1de4df9094f0
Victor <v.huang435@gmail.com>
2024-01-20 15:24:18 +1100

    updated header

--- app/(userdata)/steamid/[steamid]/_components/header.tsx
+++ app/(userdata)/steamid/[steamid]/_components/header.tsx
@@ -1,18 +1,13 @@
 "use client";
-import { cn } from "@/lib/utils";
+
 import { ChevronLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React from "react";
 
-interface HeaderProps {
-  children: React.ReactNode;
-  className?: string;
-}
-
-const Header: React.FC<HeaderProps> = ({ children, className }) => {
+const Header = () => {
   const router = useRouter();
   return (
-    <div className={cn("h-fit bg-gradient-to-b from-[#4869ff] p-6", className)}>
+    <div className={"h-fit bg-gradient-to-b from-[#4869ff] p-6"}>
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
           <button
@@ -23,7 +18,6 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           </button>
         </div>
       </div>
-      {children}
     </div>
   );
 };
--- app/(userdata)/steamid/[steamid]/page.tsx
+++ app/(userdata)/steamid/[steamid]/page.tsx
@@ -2,12 +2,10 @@ import { GameTable } from "./_components/gameTable";
 import Header from "./_components/header";
 import { UserInfo } from "./_components/userInfo";
 
-const UserPage = async ({ children }: { children: React.ReactNode }) => {
+const UserPage = async () => {
   return (
     <>
-      <Header>
-        <div></div>
-      </Header>
+      <Header />
       <div className="min-w-screen min-h-screen flex-col justify-start px-5 py-5 bg-[#202020]">
         <UserInfo />
         <GameTable />
